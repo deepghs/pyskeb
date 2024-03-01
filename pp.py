92902f25ef73c12243140cd18291bba3105ba960
narugo1992 <narugo992@gmail.com>
2024-03-02 05:43:40 +0800

    dev(narugo): run pp, ci skip

--- pp.py
+++ pp.py
@@ -8,6 +8,7 @@ from hbutils.string import plural_word
 from hbutils.system import TemporaryDirectory
 from hfutils.operate import get_hf_client, get_hf_fs, download_archive_as_directory, upload_directory_as_archive
 from hfutils.operate.base import _get_hf_token
+from tqdm import tqdm
 from waifuc.action import AlignMaxAreaAction, FileExtAction, ModeConvertAction
 from waifuc.source import LocalSource
 
@@ -19,7 +20,9 @@ hf_fs = get_hf_fs()
 all_repos = list(hf_client.list_datasets(author='StyleMuseum'))
 random.shuffle(all_repos)
 
-interval = 50
+interval = 1
+total = 2000
+pg = tqdm(desc='Total', total=total)
 
 if __name__ == '__main__':
     cnt = 0
@@ -57,6 +60,7 @@ if __name__ == '__main__':
                 )[:100].export(os.path.join(save_dir, name))
 
                 cnt += 1
+                pg.update()
                 if cnt % interval == 0:
                     archive_name = f'pack_{int(cnt // interval)}.zip'
                     upload_directory_as_archive(
