284624a127ea63e8023b494d0a551d112c908509
narugo1992 <narugo992@gmail.com>
2024-03-02 15:43:40 +0800

    dev(narugo): update README page, ci skip

--- test/prepare/mhs/newest.py
+++ test/prepare/mhs/newest.py
@@ -258,6 +258,6 @@ if __name__ == '__main__':
     logging.try_init_root(logging.INFO)
     mhs_newest_crawl(
         repository=os.environ['REMOTE_REPOSITORY_MHS_NEWEST'],
-        maxcnt=1000,
+        maxcnt=1000, 
         max_time_limit=50 * 60,
     )
